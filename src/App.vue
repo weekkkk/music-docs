559b2b4b2e32a12aa3fb3e0126ea357ee2af6337
weekkkk <nikitanedelko9@gmail.com>
2022-12-13 22:33:20 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!-- @format -->
+
+<script lang="ts" setup></script>
+
+<template>
+  <h1>@music-docs</h1>
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Montserrat';
+  font-size: var(--nc-fs-p);
+  line-height: var(--nc-lh-regular);
+  color: rgba(var(--nc-c-base), var(--nc-c-alpha));
+}
+</style>
